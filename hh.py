98f35009b522493e4d7356a3e3c66a762fbd4e70
heatcliff <awjon94@gmail.com>
2024-02-11 23:28:56 +0300

    добавил лимит на токен, немного изменил логику

--- hh.py
+++ hh.py
@@ -8,7 +8,7 @@ TOKEN = '6866886370:AAF-Xr196PsV3jJwgtljyHXxN1I0GWjPm_Y'
 bot = telebot.TeleBot(TOKEN)
 admins = [237736221]
 
-# access_token = 'USERU1PNSCDC6KAP4QQ795S7NG2E91004TIEOU63TGFVOU57AA13HHFKC0Q0U52T'
+# refresh_token = 'USERU1PNSCDC6KAP4QQ795S7NG2E91004TIEOU63TGFVOU57AA13HHFKC0Q0U52T'
 
 client_id = 'KATBUHAPQ19FBB96GTM8MV98VKGAQUP4BRM2R3G0GVBB57RRA4TNBSHQ3EH0Q0CL'
 client_secret = 'UQUEG1P6GRJJVLDL2U5PN5U58FHCLSVN1519TNARVPBRBMEVM9FPBRGAVKU457QP'
@@ -28,44 +28,24 @@ def handle_error(response_json, admin_id):
     send_message(admin_id, 'Response', error_text)
 
 
-def get_access_token():
+def get_refresh_access_token():
     try:
-        cookies = {
-            '_xsrf': '967ac1b2f5c942d92f057f67dcbcb971',
-            'hhtoken': 'V2nMzzzpCyU19E4xuTfALS2pNnIT'
-        }
-
+        with open('./refresh_token', 'r') as file:
+            refresh_token = file.read()
         headers = {
-            'content-type': 'application/x-www-form-urlencoded',
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        }
-
-        params = {
-            'response_type': 'code',
-            'client_id': 'KATBUHAPQ19FBB96GTM8MV98VKGAQUP4BRM2R3G0GVBB57RRA4TNBSHQ3EH0Q0CL',
-        }
-
-        data = {
-            'grant': '1',
-            '_xsrf': '967ac1b2f5c942d92f057f67dcbcb971',
-        }
-        response = requests.post('https://hh.ru/oauth/authorize', params=params, cookies=cookies, headers=headers,
-                                 data=data)
-        code = response.history[-1].url.split("=")[-1]
-
-        headers = {
-            'User-Agent': 'api-test-agent',
+            'grant_type': '',
             'Content-Type': 'application/x-www-form-urlencoded',
         }
 
         data = {
-            'grant_type': 'authorization_code',
-            'client_id': client_id,
-            'client_secret': client_secret,
-            'code': code,
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token,
         }
 
         response = requests.post('https://hh.ru/oauth/token', headers=headers, data=data)
+        with open('./refresh_token', 'w') as file:
+            file.write(response.json().get('refresh_token'))
+
         with open('./access_token', 'w') as file:
             file.write(response.json().get('access_token'))
 
@@ -82,12 +62,12 @@ def send_request():
                'HH-User-Agent': 'autoupdate-hh-resume/1.0 (awjon94@gmail.com)'}
 
     attempt_count = 0
-    max_attempts = 7
+    max_attempts = 5
 
     while attempt_count < max_attempts:
         r = requests.post(url, headers=headers)
         if r.status_code == 403 and r.json().get('oauth_error') == 'token-expired':
-            get_access_token()
+            get_refresh_access_token()
             attempt_count += 1
 
         elif r.status_code == 204:
