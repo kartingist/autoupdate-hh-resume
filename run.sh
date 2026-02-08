5524563c68df9857f335fd823c7939c89ffcd692
heatcliff <awjon94@gmail.com>
2026-02-09 00:08:55 +0300

    .

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Переходим в папку скрипта
-cd /root/python/autoupdate-hh-resume
-# Записываем дату
-date >> hh_cron.log
-# Запускаем питон и пишем вывод в тот же лог
-/usr/bin/python -m hh >> hh_cron.log 2>&1
