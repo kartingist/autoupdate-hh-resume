28a1ac81d00eac8c2d2db96f9f735ae921dec949
heatcliff <awjon94@gmail.com>
2026-02-08 23:51:34 +0300

    .

--- hh.py
+++ hh.py
@@ -1,94 +1,36 @@
-import sys
 import requests
-import logging
-import time
-import telebot
 
-TOKEN = '6866886370:AAF-Xr196PsV3jJwgtljyHXxN1I0GWjPm_Y'
-bot = telebot.TeleBot(TOKEN)
-admins = [237736221]
-
-# refresh_token = 'USERU1PNSCDC6KAP4QQ795S7NG2E91004TIEOU63TGFVOU57AA13HHFKC0Q0U52T'
-
-client_id = 'KATBUHAPQ19FBB96GTM8MV98VKGAQUP4BRM2R3G0GVBB57RRA4TNBSHQ3EH0Q0CL'
-client_secret = 'UQUEG1P6GRJJVLDL2U5PN5U58FHCLSVN1519TNARVPBRBMEVM9FPBRGAVKU457QP'
-resume_id = '7c896da7ff07cdf4bc0039ed1f594776395242'
-
-with open("/tmp/hh_cron_debug.log", "a") as log_file:
-    log_file.write("hh.py started from cron\n")
-
-def send_message(chat_id, title, text):
-    bot.send_message(chat_id, f"{title}:\n {text}")
+# 1. Создаем объект сессии
+session = requests.Session()
 
 
-def handle_error(response_json, admin_id):
-    description = response_json.get('description', 'Unknown error')
-    errors = response_json.get('errors', [])
-    error_messages = ', '.join([error.get('value', 'Unknown') for error in errors])
-    error_text = f"description: {description}.\n" \
-                 f" Errors: {error_messages}"
-    send_message(admin_id, 'Response', error_text)
 
+payload = {'username': 'karting-35@ya.ru',
+'password': '359325Aw',
+'accountType': 'APPLICANT',
+'failUrl': '/account/login?backurl=%2F&role=applicant',
+'remember': 'true',
+'captchaText': ''}
 
-def get_refresh_access_token():
-    try:
-        with open('/root/python/autoupdate-hh-resume/refresh_token', 'r') as file:
-            refresh_token = file.read()
-        headers = {
-            'grant_type': '',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
+headers = {
+  'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
+  'x-xsrftoken': 'aae5b8ea7ae4d75131b0d404f62b989c',
+  'Cookie': '__ddg10_=1770582496; __ddg1_=wagUYJPhHSaeqBNg4J9m; __ddg8_=XEA8Y62NOdUQUh2x; __ddg9_=193.233.122.56; _xsrf=aae5b8ea7ae4d75131b0d404f62b989c; crypted_hhuid=44ED10FB17142425C8B86C13C4234CFFB559CD7F5AD675452A02DA60CA4FC3D6; crypted_id=D785A71578CD7A474EAD7FF9B79BC125A5471C6F722104504734B32F0D03E9F8; hhrole=anonymous; hhtoken=IQGhskrtIZesvLF13LOC3r5XqLBO; hhuid=xz!wEctf7sJd_2lYDMg7DQ--; hhul=bd307c89b40b6bb3a6486d7306b3ec1ae0df8bf88e2f4f28125dd0db656c2e2b; __ddg10_=1770582467; __ddg8_=gKsS3fELq6Txu0xa; __ddg9_=193.233.122.56; _hi=39821997; display=desktop; just_logged_in=1; regions=53'
+}
 
-        data = {
-            'grant_type': 'refresh_token',
-            'refresh_token': refresh_token,
-        }
+# 2. Первый запрос (авторизация)
+# Сессия автоматически получит и сохранит куки
+auth_url = "https://krasnodar.hh.ru/account/login?backurl=%2F&role=applicant"
+response1 = session.post(auth_url, data=payload, headers=headers)
 
-        response = requests.post('https://hh.ru/oauth/token', headers=headers, data=data)
-        with open('/root/python/autoupdate-hh-resume/refresh_token', 'w') as file:
-            file.write(response.json().get('refresh_token'))
+# Проверяем, что авторизация прошла успешно
+if response1.status_code == 200:
+    print("Авторизация успешна!")
 
-        with open('/root/python/autoupdate-hh-resume/access_token', 'w') as file:
-            file.write(response.json().get('access_token'))
+    # 3. Второй запрос (сама команда)
+    # Куки из response1 подставятся автоматически
+    command_url = 'https://krasnodar.hh.ru/applicant/resumes/touch'
+    payload = {'resume': '7c896da7ff07cdf4bc0039ed1f594776395242',
+               'undirectable': 'true'}
+    response2 = session.post(command_url, data=payload, headers=headers)
 
-    except:
-        for admin in admins:
-            send_message(chat_id=admin, title="Ошибка обновления токена", text='...')
-
-
-def send_request():
-    with open('/root/python/autoupdate-hh-resume/access_token', 'r') as file:
-        access_token = file.read()
-    url = f'https://api.hh.ru/resumes/{resume_id}/publish'
-    headers = {'Authorization': f'Bearer {access_token}',
-               'HH-User-Agent': 'autoupdate-hh-resume/1.0 (awjon94@gmail.com)'}
-
-    attempt_count = 0
-    max_attempts = 5
-
-    while attempt_count < max_attempts:
-        r = requests.post(url, headers=headers)
-        if r.status_code == 403 and r.json().get('oauth_error') == 'token-expired':
-            get_refresh_access_token()
-            attempt_count += 1
-
-        elif r.status_code == 204:
-            for admin in admins:
-                send_message(admin, 'Success', 'Your resume https://hh.ru/resume/' + resume_id + ' is updated')
-            return
-
-        elif r.status_code in [429, 403, 404, 400]:
-            for admin in admins:
-                send_message(admin, 'Error', f'Problem with updating resume. Status code: {r.status_code}')
-                handle_error(r.json(), admin)
-            attempt_count += 1
-            time.sleep(60)
-        else:
-            break
-
-    for admin in admins:
-        send_message(admin, 'Error', f'Failed to update resume after {max_attempts} attempts')
-
-
-if __name__ == '__main__':
-    send_request()
